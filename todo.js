c2ac8378d3059076e20c02d1e830be45afda555e
baro <knabao7a7@gmail.com>
2021-06-21 23:10:13 +0700

    responsive finished

--- todo.js
+++ todo.js
@@ -13,7 +13,32 @@ setInterval(()=> {
     index=0;
 },100)
 
+addBtn.addEventListener("click",function(){
+    if (!test(input.value))
+        return false; 
+    else   {
+    var inputValue=input;
+    inputValue=document.createTextNode(input.value);
+    var li=document.createElement("li");
+    li.appendChild(inputValue);
+    ul.appendChild(li);
+    input.value=""; 
+    }   
+    event.preventDefault();
+})
 
+function test(input){
+    if (input==="")
+        return false;
+    else
+        return true;
+}
+
+
+removeBtn.addEventListener("click",function(){
+    var li = document.querySelector(".todo-list li");
+    ul.removeChild(li);
+})
 
 const changetheme = document.querySelector(".change-theme");
 changetheme.addEventListener("click",()=>{
